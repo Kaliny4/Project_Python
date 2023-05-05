2c1f2d94b5c016203fd505aa70da2559d4bd070a
hanna <hanna.kalinichenko@gmail.com>
2023-05-05 23:39:07 +0200

    func

--- 2.py
+++ 2.py
@@ -58,7 +58,7 @@ def main():
         if not (
             user_input := int(
                 input(
-                f"Enter your the number of action {choice}: "
+                    f"Enter your the number of action {choice}: "
                     )
                 )
         ):
@@ -74,7 +74,7 @@ def main():
         if user_input == 4:
             players_find(
                 input("Enter the parameter (name, age or number): "),
-                input("Enter the value of " /
+                input("Enter the value of " / 
                       "the parameter (f.e. Peter or 20): "),
                 team,
             )
