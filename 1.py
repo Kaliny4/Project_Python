b8f638371a2e605b8f8dcc58fafe0a9a7bb0d738
hanna <hanna.kalinichenko@gmail.com>
2023-05-06 01:35:52 +0200

    func

--- 1.py
+++ 1.py
@@ -25,7 +25,7 @@ def count_print():
     file_obj.writelines(results)
     file_obj.close()
 
-    with open("results.txt", "r") as fhttps://github.com/Kaliny4/Project_Python.gitp:
+    with open("results.txt", "r") as fp:
         x = len(fp.readlines())
         print("Total lines:", x)
         print("Size: ", asizeof.asizeof("results.txt"))
